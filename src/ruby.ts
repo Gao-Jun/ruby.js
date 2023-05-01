a3a7c04babce8c72a291f238501da54d9ab4a987
gaojun <gaojun@fudan.edu.cn>
2023-05-01 18:02:32 +0800

    make ruby() function signature clearer

--- src/ruby.ts
+++ src/ruby.ts
@@ -1,20 +1,27 @@
 import RubyObject from './rubyObject.js';
 import RubyString from './rubyString.js';
 import RubyEnumerable from './rubyEnumerable.js';
+import RubyArray from "./rubyArray.js";
 
-type RubyType<T> = T extends string ? RubyString :
-                   T extends Iterable<any> ? RubyEnumerable : never;
+function ruby<V>(js: string, func: (rubyObj: RubyString) => RubyObject<V>): V;
+function ruby<T, V>(js: Array<T>, func: (rubyObj: RubyArray<T>) => RubyObject<V>): V;
+function ruby<V>(js: Iterable<any>, func: (rubyObj: RubyEnumerable) => RubyObject<V>): V;
 
-function ruby<T,V>(js: T, func: (rubyObj: RubyType<T>) => RubyObject<V>): V {
+function ruby<V>(js: any, func: (rubyObj: any) => RubyObject<V>): V {
+    let rubyObj = null;
     if (typeof(js) === 'string'){
-        const rubyObj = new RubyString(js);
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        return func(rubyObj as any).toJS();
+        rubyObj = new RubyString(js);
+    } else if (Array.isArray(js)) {
+        rubyObj = new RubyArray(js);
     } else if (js !== null && js !== undefined && typeof((js as any)[Symbol.iterator]) === 'function') {
-        const rubyObj = new RubyEnumerable(js as any);
-        return func(rubyObj as any).toJS();
+        rubyObj = new RubyEnumerable(js as any);
     }
-    throw new TypeError(`Cannot convert ${js} to Ruby type.`);
+
+    if (rubyObj) {
+        return func(rubyObj).toJS();
+    } else {
+        throw new TypeError(`Cannot convert ${js} to Ruby type.`);
+    }
 }
 
 export default ruby;
