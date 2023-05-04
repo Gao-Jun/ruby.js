ed0f88d01f0c2a73b61f5351235b1280669de1a6
gaojun <gaojun@fudan.edu.cn>
2023-05-04 12:51:07 +0800

    Add ruby(() => {}) method

--- src/ruby.ts
+++ src/ruby.ts
@@ -6,18 +6,23 @@ import RubyArray from "./rubyArray.js";
 function ruby<V>(js: string, func: (rubyObj: RubyString) => RubyObject<V>): V;
 function ruby<T, V>(js: Array<T>, func: (rubyObj: RubyArray<T>) => RubyObject<V>): V;
 function ruby<V>(js: Iterable<any>, func: (rubyObj: RubyEnumerable) => RubyObject<V>): V;
+function ruby<V>(func: () => RubyObject<V>): V;
 
-function ruby<V>(js: any, func: (rubyObj: any) => RubyObject<V>): V {
+function ruby<V>(js_or_func: any, func?: (rubyObj?: any) => RubyObject<V>): V {
+    if (typeof(js_or_func) === 'function') {
+        return js_or_func().toJS();
+    }
+    const js = js_or_func;
     let rubyObj = null;
     if (typeof(js) === 'string'){
         rubyObj = new RubyString(js);
     } else if (Array.isArray(js)) {
         rubyObj = new RubyArray(js);
     } else if (js !== null && js !== undefined && typeof((js as any)[Symbol.iterator]) === 'function') {
-        rubyObj = new RubyEnumerable(js as any);
+        rubyObj = new RubyEnumerable(js);
     }
 
-    if (rubyObj) {
+    if (rubyObj && func) {
         return func(rubyObj).toJS();
     } else {
         throw new TypeError(`Cannot convert ${js} to Ruby type.`);
